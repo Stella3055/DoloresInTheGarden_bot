574d5839cc16ec942ea780e5f0a1753d05e0f6f9
Stella3055 <raven_uw@yahoo.com>
2020-09-15 18:39:42 +0800

    switch to heroku

--- bot.py
+++ bot.py
@@ -1,19 +1,46 @@
-import time
-import telepot
-from pprint import pprint
-from telepot.loop import MessageLoop
+from telegram.ext import Updater, CommandHandler
+import os
 
-TOKEN = os.environ["TOKEN"]
 
-bot = telepot.Bot(TOKEN)
-pprint(bot.getMe())
-def handle(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    print(content_type, chat_type, chat_id)
-    if msg['text'] == '/food':
-        bot.sendMessage(chat_id, 'hamburger')
-    elif msg['text'] == '/drink':
-        bot.sendMessage(chat_id, 'coke zero')
-MessageLoop(bot, handle).run_as_thread()
-while 1:
-    time.sleep(10)
+# 設定一些個人的環境變數
+# TOKEN = os.environ["TOKEN"]
+TOKEN = "1305894368:AAFJLlTmfB58_xmkR-HGz4NRo9o1kDUAy_M"
+PORT = int(os.environ.get('PORT', '8443'))
+
+
+updater = Updater(token=TOKEN)  # 呼叫 bot 用
+
+
+def welcome(bot, update):
+	""" Show user welcome message. """
+	
+	chat_id = update.message.from_user.id
+	
+	about_bot = ''
+	about_bot = about_bot + 'Hello! 感謝您的使用\n'
+	about_bot = about_bot + '本機器人由 [hms5232](https://medium.com/@hms5232) 開發\n'
+	about_bot = about_bot + '用於將機器人部署在 Heroku 上的教學文的範例\n'
+	about_bot = about_bot + '可於 [Github](https://github.com/hms5232) 上找到我\n'
+	about_bot = about_bot + '文章原文可在[repo簡介](https://github.com/hms5232/deploy-python-telegram-bot-on-heroku)上找到\n'
+	
+	bot.send_message(chat_id, about_bot, parse_mode='Markdown')
+
+
+def hello(bot, update):
+	""" Hello World! """
+	
+	update.message.reply_text('Hello world!')
+
+
+updater.dispatcher.add_handler(CommandHandler('start', welcome))  # 歡迎訊息
+updater.dispatcher.add_handler(CommandHandler('hi', hello))  # Hello World!
+
+
+# 執行機器人必須要的，讓機器人運作聽命
+#updater.start_polling()
+# 改用 webhook 的方式
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://dolores-in-the-garden-bot.herokuapp.com/" + TOKEN)
+updater.idle()
