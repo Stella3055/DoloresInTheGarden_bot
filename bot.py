479d4069d635e1e586ed81b980db7949303ab57f
Stella3055 <raven_uw@yahoo.com>
2020-09-15 17:58:28 +0800

    init

---
+++ bot.py
@@ -0,0 +1,24 @@
+import time
+import telepot
+from pprint import pprint
+from telepot.loop import MessageLoop
+
+try:
+    with open("token") as tokenFile:
+        TOKEN = tokenFile.read()
+except:
+    print("Token file not set yet!")
+
+
+bot = telepot.Bot(TOKEN)
+pprint(bot.getMe())
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+    if msg['text'] == '/food':
+        bot.sendMessage(chat_id, 'hamburger')
+    elif msg['text'] == '/drink':
+        bot.sendMessage(chat_id, 'coke zero')
+MessageLoop(bot, handle).run_as_thread()
+while 1:
+    time.sleep(10)
