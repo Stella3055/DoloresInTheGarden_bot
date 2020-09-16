5e45448158c72be87faa7f3ed164c4ecbfe55696
Stella3055 <raven_uw@yahoo.com>
2020-09-16 17:04:57 +0800

    switch to bot code

--- bot.py
+++ bot.py
@@ -1,46 +1,46 @@
-# import telegram
-# from flask import Flask, request
-# from telegram.ext import Dispatcher, MessageHandler, Filters
-# import os
+import telegram
+from flask import Flask, request
+from telegram.ext import Dispatcher, MessageHandler, Filters
+import os
 
-# TOKEN = os.environ["TOKEN"]
-# PORT = int(os.environ.get('PORT', '5000'))
-# bot = telegram.Bot(token=TOKEN)
-# app = Flask(__name__)
-
-# @app.route('/hook', methods=['POST'])
-# def webhook_handler():
-#     """Set route /hook with POST method will trigger this method."""
-#     if request.method == "POST":
-#         update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-#         # Update dispatcher process that handler to process this message
-#         dispatcher.process_update(update)
-#     return 'ok'
-
-
-# def reply_handler(bot, update):
-#     """Reply message."""
-#     text = update.message.text
-#     update.message.reply_text(text)
-
-
-# # New a dispatcher for bot
-# dispatcher = Dispatcher(bot, None)
-
-# # Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
-# # message.
-# dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
-
-# if __name__ == "__main__":
-#     # Running server
-#     app.run(port = PORT, host='0.0.0.0')
-from flask import Flask
+TOKEN = os.environ["TOKEN"]
+PORT = int(os.environ.get('PORT', '5000'))
+bot = telegram.Bot(token=TOKEN)
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.route('/hook', methods=['POST'])
+def webhook_handler():
+    """Set route /hook with POST method will trigger this method."""
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+        # Update dispatcher process that handler to process this message
+        dispatcher.process_update(update)
+    return 'ok'
+
+
+def reply_handler(bot, update):
+    """Reply message."""
+    text = update.message.text
+    update.message.reply_text(text)
+
+
+# New a dispatcher for bot
+dispatcher = Dispatcher(bot, None)
+
+# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
+# message.
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+
+if __name__ == "__main__":
+    # Running server
+    app.run(port = PORT, host='0.0.0.0')
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return 'Hello World!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
