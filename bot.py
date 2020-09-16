8a69c01ee49c3fd86c6681b7c2c3708a9e4771b7
Stella3055 <raven_uw@yahoo.com>
2020-09-16 10:33:10 +0800

    20200916-2

--- bot.py
+++ bot.py
@@ -1,38 +1,36 @@
-from telegram.ext import Updater, CommandHandler
+import telegram
+from flask import Flask, request
+from telegram.ext import Dispatcher, MessageHandler, Filters
 import os
 
-# 設定一些個人的環境變數
 TOKEN = os.environ["TOKEN"]
+bot = telegram.Bot(token=TOKEN)
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def webhook_handler():
+    """Set route / with POST method will trigger this method."""
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+        # Update dispatcher process that handler to process this message
+        dispatcher.process_update(update)
+    return 'ok'
 
 
-updater = Updater(token=TOKEN)  # 呼叫 bot 用
+def reply_handler(bot, update):
+    """Reply message."""
+    text = update.message.text
+    update.message.reply_text(text)
 
 
-def welcome(bot, update):
-	""" Show user welcome message. """
-	
-	chat_id = update.message.from_user.id
-	
-	about_bot = ''
-	about_bot = about_bot + 'Hello! 感謝您的使用\n'
-	about_bot = about_bot + '本機器人由 [hms5232](https://medium.com/@hms5232) 開發\n'
-	about_bot = about_bot + '用於將機器人部署在 Heroku 上的教學文的範例\n'
-	about_bot = about_bot + '可於 [Github](https://github.com/hms5232) 上找到我\n'
-	about_bot = about_bot + '文章原文可在[repo簡介](https://github.com/hms5232/deploy-python-telegram-bot-on-heroku)上找到\n'
-	
-	bot.send_message(chat_id, about_bot, parse_mode='Markdown')
+# New a dispatcher for bot
+dispatcher = Dispatcher(bot, None)
 
+# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
+# message.
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
 
-def hello(bot, update):
-	""" Hello World! """
-	
-	update.message.reply_text('Hello world!')
-
-
-updater.dispatcher.add_handler(CommandHandler('start', welcome))  # 歡迎訊息
-updater.dispatcher.add_handler(CommandHandler('hi', hello))  # Hello World!
-
-updater.start_webhook(listen="0.0.0.0",
-                      url_path=TOKEN)
-updater.bot.set_webhook("https://dolores-in-the-garden-bot.herokuapp.com/" + TOKEN)
-updater.idle()
+if __name__ == "__main__":
+    # Running server
+    app.run(debug=True)
